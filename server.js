6950f5b719a80edd0a6b299da8ee9d20309251bf
thebasix <asamuga@gmail.com>
2016-04-04 12:44:11 +0200

    Correct req.headers

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let app = express();
 const PORT = process.env.PORT || 3030;
 
 app.use((req, res, next) => {
-    if (res.headers['x-forwarded-proto'] === 'http') {
+    if (req.headers['x-forwarded-proto'] === 'http') {
         next();
     } else {
         res.redirect('http://' + req.hostname + req.url);
